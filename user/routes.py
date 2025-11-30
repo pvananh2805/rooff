23c8d5cd4e25d249e194fd59ca4c5815f36ec115
pvananh2805 <pvananh2805@gmail.com>
2025-12-01 01:28:13 +0700

    Add files via upload

---
+++ user/routes.py
@@ -0,0 +1,14 @@
+# user/routes.py
+from flask import Blueprint, render_template, abort
+from flask_login import login_required
+from models import User
+
+user_bp = Blueprint("user", __name__)
+
+@user_bp.route("/<handle>")
+@login_required
+def profile(handle):
+    user = User.query.filter_by(handle=handle).first()
+    if not user:
+        abort(404)
+    return render_template("user_profile.html", user=user)
